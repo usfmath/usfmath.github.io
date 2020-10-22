bb1d5bd596185fb7a39740047d09909aae8cb969
usfmath <fudong@usf.edu>
2020-10-22 15:30:06 -0400

    rebuilding site Thu Oct 22 15:30:06 EDT 2020

---
+++ talk/Dirchlet-Problem.m
@@ -0,0 +1,40 @@
+---
+# Documentation: https://sourcethemes.com/academic/docs/managing-content/
+
+title: "On solving the Dirichlet problem"
+subtitle: ""
+summary: "**Speaker**: Louis Arenas,
+
+scheduled at 11:00 pm, Friday, October 23rd, 2020 @ [zoom](https://zoom.us/j/92707416063)
+"
+authors: []
+tags: ["PDE","Dirchlet Problem"]
+categories: ["Classical analysis"]
+
+# Featured image
+# To use, add an image named `featured.jpg/png` to your page's folder.
+# Focal points: Smart, Center, TopLeft, Top, TopRight, Left, Right, BottomLeft, Bottom, BottomRight.
+image:
+  caption: ""
+  focal_point: ""
+  preview_only: false
+
+# Projects (optional).
+#   Associate this post with one or more of your projects.
+#   Simply enter your project's folder or file name without extension.
+#   E.g. `projects = ["internal-project"]` references `content/project/deep-learning/index.md`.
+#   Otherwise, set `projects = []`.
+projects: []
+---
+
+
+**Title**: On solving the Dirichlet problem
+
+**Speaker**: Louis Arenas
+
+**Date**: October 23rd
+
+**Time**: 11:00 pm
+
+**Abstract**: The talk will start by reviewing the mainstream strategies for solving the problem on simply connected domains, with particular attention to the disk. It is hoped that we have enough time to talk about Malmheden's strategy for solving the problem on the disk. The talk should be of interest to anyone taking or wanting a review of basic concepts in complex analysis.
+
